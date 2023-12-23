27d40286435f9ea681acbf2df23fc3ea2244f60a
mame <edamame15.r@gmail.com>
2023-12-23 20:34:44 +0900

    template.json

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 export PATH="$PATH:$PWD"
 export ROOTDIR=`pwd`
 cp ./template.hs ~/.config/atcoder-cli-nodejs/haskell/main.hs
+cp ./template.json ~/.config/atcoder-cli-nodejs/haskell/template.json
 
